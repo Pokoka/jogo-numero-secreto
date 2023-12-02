6b322bdb31489c8a88d39d363f8626e2bf8652d1
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 13:10:30 -0300

    concatenação e condicionais

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 alert("Bem Vindos ao jogo do número secreto!");
-let numeroSecreto = 29;
+let numeroSecreto = 2;
+
+console.log (numeroSecreto);
+
 let numeroEscolhido = prompt("Escolha um número entre 1 e 30:");
 
+// se o nnúmero escolhido for igual ao número secreto
 if (numeroSecreto == numeroEscolhido) {
-    console.log ("Você descobriu o número secreto (29)");
+   // alert ("Parabéns! Você descobriu o número secreto: " + numeroSecreto + "!");
+    alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto}!`);
+} else {
+    alert ("Que pena, você errou :(");
 }
