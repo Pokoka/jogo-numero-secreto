323428b006ba4a520914a01abe61700eb742d4d4
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 16:00:55 -0300

    Loop com while e contador de tentativas

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 alert("Bem Vindos ao jogo do número secreto!");
-let numeroSecreto = 2;
-
+let numeroSecreto = 29;
 console.log (numeroSecreto);
+let numeroEscolhido
+let contadorTentativas = 1;
 
-let numeroEscolhido = prompt("Escolha um número entre 1 e 30:");
-
-// se o nnúmero escolhido for igual ao número secreto
-if (numeroSecreto == numeroEscolhido) {
-   // alert ("Parabéns! Você descobriu o número secreto: " + numeroSecreto + "!");
-    alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto}!`);
-} else {
-    alert ("Que pena, você errou :(");
+// enquanto o numero não for igual ao númer secreto
+while (numeroEscolhido != numeroSecreto) {
+    numeroEscolhido = prompt ("Escolha um número entre 1 e 30:");
+    
+    // se o nnúmero escolhido for igual ao número secreto
+    if (numeroSecreto == numeroEscolhido) {
+        // alert ("Parabéns! Você descobriu o número secreto: " + numeroSecreto + "!");
+         alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativa(s)!`);
+    } else {
+        if (numeroEscolhido > numeroSecreto) {
+            alert (`O número secreto é maenor que ${numeroEscolhido}!`);
+        } else {
+            alert (`O número secreto é maior qur ${numeroEscolhido}!`);
+        }
+        // ++ incrementa em 1 o valor
+        contadorTentativas ++;
+     } 
 }
