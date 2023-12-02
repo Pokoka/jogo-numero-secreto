ece2dda9932df375ae4c9a1f0552022aaeb54f20
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 16:29:09 -0300

    gerar número de 1 a 100, operador ternário

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 alert("Bem Vindos ao jogo do número secreto!");
-let numeroSecreto = 29;
+let numeroSecreto = parseInt (Math.random() * 100 + 1);
 console.log (numeroSecreto);
 let numeroEscolhido
 let contadorTentativas = 1;
 
 // enquanto o numero não for igual ao númer secreto
 while (numeroEscolhido != numeroSecreto) {
-    numeroEscolhido = prompt ("Escolha um número entre 1 e 30:");
+    numeroEscolhido = prompt ("Escolha um número entre 1 e 100:");
     
     // se o nnúmero escolhido for igual ao número secreto
     if (numeroSecreto == numeroEscolhido) {
+        // não continua
+        break;
         // alert ("Parabéns! Você descobriu o número secreto: " + numeroSecreto + "!");
-         alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativa(s)!`);
     } else {
         if (numeroEscolhido > numeroSecreto) {
             alert (`O número secreto é maenor que ${numeroEscolhido}!`);
@@ -22,3 +23,12 @@ while (numeroEscolhido != numeroSecreto) {
         contadorTentativas ++;
      } 
 }
+
+let palavraTentativa = contadorTentativas > 1 ? "tentativas" : "tentativa";
+alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} ${palavraTentativa}!`);
+
+//if (contadorTentativas > 1) {
+  //  alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativas!`);
+//} else {
+  //  alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativa!`);
+//}
