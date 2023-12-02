2b7f1ad0a5d0e76ee2ed3ac2270a035fddcadf3c
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 12:18:46 -0300

    atualização arquivo java
    adição de comandos alert e prompt; iniciando as condicinais.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert("Bem Vindos ao jogo do número secreto!");
 let numeroSecreto = 29;
-prompt("Escolha um número entre 1 e 30:");
+let numeroEscolhido = prompt("Escolha um número entre 1 e 30:");
+
+if (numeroSecreto == numeroEscolhido) {
+    console.log ("Você descobriu o número secreto (29)");
+}
