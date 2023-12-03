c713c3353363418f2da5db4893860dcd50ebd4e0
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 23:09:24 -0300

    funcões /  booleano / gerar numeros aleatorios

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número secreto";
+// let titulo = document.querySelector("h1");
+//titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número entre 1 e 100:";
+//let paragrafo = document.querySelector("p");
+//paragrafo.innerHTML = "Escolha um número entre 1 e 100:";
+let numeroSecreto = gerarNumeroAleatorio ();
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela ("h1", "Jogo do número secreto");
+exibirTextoNaTela ("p", "Escolha um número entre 1 e 100:");
+console.log (numeroSecreto);
 
 function verificarNumeroEscolhido() {
+    let numeroEscolhido = document.querySelector ("input").value;
     console.log ("O botão foi clicado");
+    console.log (numeroEscolhido == numeroSecreto);
+}
+
+function gerarNumeroAleatorio () {
+   return parseInt (Math.random () * 100 +1);
 }
