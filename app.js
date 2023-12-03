5e2887040bd614d6977821f33fa87c0247ccc15a
Pokoka <138378312+Pokoka@users.noreply.github.com>
2023-12-02 22:34:24 -0300

    Novas configurações
    Layout diferente e execução fora de alerta e prompt / uso de funções / manipular HTML.

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
-alert("Bem Vindos ao jogo do número secreto!");
-let numeroSecreto = parseInt (Math.random() * 100 + 1);
-console.log (numeroSecreto);
-let numeroEscolhido
-let contadorTentativas = 1;
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número secreto";
 
-// enquanto o numero não for igual ao númer secreto
-while (numeroEscolhido != numeroSecreto) {
-    numeroEscolhido = prompt ("Escolha um número entre 1 e 100:");
-    
-    // se o nnúmero escolhido for igual ao número secreto
-    if (numeroSecreto == numeroEscolhido) {
-        // não continua
-        break;
-        // alert ("Parabéns! Você descobriu o número secreto: " + numeroSecreto + "!");
-    } else {
-        if (numeroEscolhido > numeroSecreto) {
-            alert (`O número secreto é maenor que ${numeroEscolhido}!`);
-        } else {
-            alert (`O número secreto é maior qur ${numeroEscolhido}!`);
-        }
-        // ++ incrementa em 1 o valor
-        contadorTentativas ++;
-     } 
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 100:";
+
+function verificarNumeroEscolhido() {
+    console.log ("O botão foi clicado");
 }
-
-let palavraTentativa = contadorTentativas > 1 ? "tentativas" : "tentativa";
-alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} ${palavraTentativa}!`);
-
-//if (contadorTentativas > 1) {
-  //  alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativas!`);
-//} else {
-  //  alert (`Parabéns! Você descobriu o número secreto: ${numeroSecreto} com ${contadorTentativas} tentativa!`);
-//}
